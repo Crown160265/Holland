29a1d7bbd01662f71629f9d8fe69ceca97231edb
www <www@test.com>
2024-08-07 12:03:45 -0400

    Holland Project

---
+++ resources/views/partial/offcanvas-menu.blade.php
@@ -0,0 +1,48 @@
+ <!-- offcanvas Menu Start -->
+ <div class="offcanvas offcanvas-end offcanvas-menu bg-white" id="offcanvasMenu">
+        <div class="offcanvas-header justify-content-end">
+            <button type="button" class="btn-close text-white" data-bs-dismiss="offcanvas"><i class="lastudioicon-e-remove"></i></button>
+        </div>
+        <div class="offcanvas-body">
+            <ul class="mobile-primary-menu">
+                <li>
+                    <a href="/" class="menu-item-link active"><span>Home</span></a>
+                </li>
+                <li class="position-static">
+                    <a class="menu-item-link" href="/"><span>Kermis snoepfabriek</span></a>
+                </li>
+                <li><a class="menu-item-link" href="/"><span>Nieuw</span></a>
+                </li>
+                <li><a class="menu-item-link" href="/"><span>Assortment</span></a>
+                    <ul class="sub-menu">
+                        <li><a class="sub-item-link" href="/collection/candy"><span>Candy</span></a></li>
+                        <li><a class="sub-item-link" href="/collection/diary"><span>Diary</span></a></li>
+                    </ul>
+                </li>
+                <li><a class="menu-item-link" href="/"><span>Verkooppunten</span></a></li>
+                <li><a class="menu-item-link" href="/"><span>Team Holland Foodz</span></a></li>
+                <li><a class="menu-item-link" href="/"><span>Contact</span></a></li>
+            </ul>
+            <ul class="hotline-wrapper offcanvas-hotline">
+                <li>
+                    <div class="hotline">
+                        <i class="lastudioicon lastudioicon-support248"></i>
+                        <div class="hotline-content">
+                            <span class="hotline-text">Hotline</span>
+                            <a class="hotline-link" href="tel:0123456789">(012) 345-6789</a>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="hotline">
+                        <i class="lastudioicon lastudioicon-pin-check"></i>
+                        <div class="hotline-content">
+                            <span class="hotline-text">Store Location</span>
+                            <a class="hotline-link" href="#/">6391 Elgin St. Celina, Delaware 10299</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- offcanvas Menu End -->
